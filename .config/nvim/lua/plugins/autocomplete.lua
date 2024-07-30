78f78bae4c2d05324713920d90ab7fd57a159bb1
Felix Geelhaar <felix@felixgeelhaar.de>
2024-07-30 23:07:13 +0200

    [bug]: disabled lua_ls formatting to avoid conflicting formatting with stylua

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -1,84 +1,84 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "onsails/lspkind.nvim",
-    {
-      "L3MON4D3/LuaSnip",
-      dependencies = {
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
-      },
-      build = (function()
-        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-          return
-        end
-        return "make install_jsregexp"
-      end)(),
-    },
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    local lspkind = require("lspkind")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"onsails/lspkind.nvim",
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = {
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+			build = (function()
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+		},
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      completion = {
-        completeopt = "menu,menuone,noinsert",
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      formatting = {
-        expandable_indicator = true,
-        fields = { "kind", "abbr" },
-        format = lspkind.cmp_format({
-          mode = "symbol",
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
-      mapping = cmp.mapping.preset.insert({
-        -- Select the [n]ext item
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        -- Accept ([y]es) the completion.
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        -- Manually trigger a completion from nvim-cmp.
-        ["<C-Space>"] = cmp.mapping.complete({}),
-        -- jump to next location
-        ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { "i", "s" }),
-        -- jump to previous location
-        ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { "i", "s" }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- lsp
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
-    })
-  end,
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			formatting = {
+				expandable_indicator = true,
+				fields = { "kind", "abbr" },
+				format = lspkind.cmp_format({
+					mode = "symbol",
+					maxwidth = 50,
+					ellipsis_char = "...",
+				}),
+			},
+			mapping = cmp.mapping.preset.insert({
+				-- Select the [n]ext item
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				-- Select the [p]revious item
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				-- Accept ([y]es) the completion.
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				-- Manually trigger a completion from nvim-cmp.
+				["<C-Space>"] = cmp.mapping.complete({}),
+				-- jump to next location
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { "i", "s" }),
+				-- jump to previous location
+				["<C-h>"] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" }, -- lsp
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+			}),
+		})
+	end,
 }
--- .config/nvim/lua/plugins/lsp_lang_settings/ansible.lua
+++ .config/nvim/lua/plugins/lsp_lang_settings/ansible.lua
@@ -2,19 +2,19 @@ return {
   settings = {
     ansible = {
       ansible = {
-        path = 'ansible',
+        path = "ansible",
       },
       executionEnvironment = {
         enabled = false,
       },
       python = {
-        interpreterPath = 'python3',
+        interpreterPath = "python3",
       },
       validation = {
         enabled = true,
         lint = {
           enabled = true,
-          path = 'ansible-lint',
+          path = "ansible-lint",
         },
       },
     },
