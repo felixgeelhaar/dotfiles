0b43f1f09f4a862923384181085991dfe904b92c
Felix Geelhaar <felix@felixgeelhaar.de>
2025-05-15 21:48:48 +0200

    feat: Implement blink.cmp and replace nvim-cmp

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -1,85 +1,37 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"onsails/lspkind.nvim",
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"saadparwaiz1/cmp_luasnip",
-				"rafamadriz/friendly-snippets",
-			},
-			build = (function()
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-		},
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+  "saghen/blink.cmp",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+  version = "1.*",
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = "default" },
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			completion = {
-				completeopt = "menu,menuone,noinsert",
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			formatting = {
-				expandable_indicator = true,
-				fields = { "kind", "abbr" },
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
-			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				-- Accept ([y]es) the completion
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				-- Manually trigger a completion from nvim-cmp
-				["<C-Space>"] = cmp.mapping.complete({}),
-				-- Jump to next location
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				-- Jump to previous location
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp",      priority = 1000 }, -- LSP
-				{ name = "luasnip",       priority = 750 }, -- Snippets
-				{ name = "buffer",        priority = 500 }, -- Text within current buffer
-				{ name = "path",          priority = 250 }, -- File system paths
-				{ name = "codecompanion", priority = 800 }, -- AI code suggestions
-			}),
-		})
-	end,
+    appearance = {
+      nerd_font_variant = "mono",
+      -- You can enhance UI with lspkind integration
+      -- formatting = {
+      --   format = require("lspkind").cmp_format({
+      --     mode = "symbol_text",
+      --     menu = {
+      --       buffer = "[Buffer]",
+      --       nvim_lsp = "[LSP]",
+      --       luasnip = "[Snippet]",
+      --       path = "[Path]",
+      --       nvim_lua = "[Lua]",
+      --     },
+      --   }),
+      -- },
+    },
+    signature = { enabled = true },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    fuzzy = { implementation = "prefer_rust" },
+  },
+  opts_extend = { "sources.default" },
 }
